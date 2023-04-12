eba0a7789500b240113020204fd08ada8a335ad7
kdan2944 <kdan2944gmail.com>
2023-04-12 12:59:26 +0300

    Add Person class extending Nameable

--- person.rb
+++ person.rb
@@ -1,16 +1,22 @@
-class Person
-  attr_accessor :name, :age
+require './nameable'
+
+class Person < Nameable
+  attr_accessor :age
   attr_reader :id
 
   def initialize(age, parent_permission: true, name: 'Unknown')
+    super(name)
     @id = rand(1..1000)
-    @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
