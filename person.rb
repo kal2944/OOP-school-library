fcc20f2b803e9639396cd5e16246ff6a13bf33c9
kdan2944 <kdan2944gmail.com>
2023-04-12 17:43:31 +0300

    fixed

--- person.rb
+++ person.rb
@@ -1,10 +1,10 @@
 require './nameable'
 
 class Person < Nameable
-  attr_accessor :age
+  attr_accessor :age, :name
   attr_reader :id
 
-  def initialize(age, parent_permission: true, name: 'Unknown')
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = rand(1..1000)
     @age = age
@@ -26,3 +26,6 @@ class Person < Nameable
     @age >= 18
   end
 end
+
+person = Person.new(22, 'shedrackstormfield')
+puts person.name
