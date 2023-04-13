56578a656fbaa5dc874a2d5fe0b6282e9f98f4b2
kdan2944 <kdan2944gmail.com>
2023-04-13 10:42:36 +0300

    Move decorators to main

--- base_decorator.rb
+++ base_decorator.rb
@@ -12,3 +12,15 @@ class Decorator < Nameable
     @nameable.correct_name
   end
 end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.length > 10 ? "#{@nameable.correct_name[0..10]}..." : @nameable.correct_name
+  end
+end
