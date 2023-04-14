60524ed00de9210953c6e2de3b431bcb91d8c23d
kdan2944 <kdan2944gmail.com>
2023-04-14 11:13:20 +0300

    Modify student.rb file

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative './person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, parent_permission: true, name: 'Unknown')
-    super(age, parent_permission: parent_permission, name: name)
+  def initialize(classroom, age, name, parent_permission)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
