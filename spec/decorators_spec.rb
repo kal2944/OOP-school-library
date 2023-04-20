36799f25a51298c878c7b5e3524f5dd3302faefc
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-20 17:46:26 +0300

    Add changes requested

--- spec/decorators_spec.rb
+++ spec/decorators_spec.rb
@@ -1,28 +1,36 @@
 require_relative 'imports'
 
-RSpec.describe Classroom do
-  let(:classroom) { Classroom.new('Math 101') }
-  let(:student1) { Student.new(1, 16, classroom.label) }
-  let(:student2) { Student.new(2, 17, classroom.label) }
-  describe '#initialize' do
-    it 'creates a new classroom with the given label and an empty array of students' do
-      expect(classroom.label).to eq('Math 101')
-      expect(classroom.students).to eq([])
+RSpec.describe Decorator do
+  describe '#correct_name' do
+    it 'returns the correct name for a person' do
+      person = Person.new('1', 25, 'John Doe')
+      decorator = Decorator.new(person)
+      expect(decorator.correct_name).to eq('John Doe')
     end
   end
-  describe '#add_student' do
-    it 'adds the given student to the classroom and sets the student\'s classroom attribute' do
-      classroom.add_student(student1)
-      expect(classroom.students).to eq([student1])
-      expect(student1.classroom).to eq(classroom)
-      classroom.add_student(student2)
-      expect(classroom.students).to eq([student1, student2])
-      expect(student2.classroom).to eq(classroom)
+  describe '#nameable attribute' do
+    it 'returns the nameable attribute of the decorator' do
+      person = Person.new('1', 25, 'John Doe')
+      decorator = Decorator.new(person)
+      expect(decorator.nameable).to eq(person)
     end
-    it 'does not add the same student to the classroom more than once' do
-      classroom.add_student(student1)
-      classroom.add_student(student1)
-      expect(classroom.students).to eq([student1])
+  end
+end
+RSpec.describe CapitalizeDecorator do
+  describe '#correct_name' do
+    it 'returns the correct capitalized name for a person' do
+      person = Person.new('1', 25, 'John Doe')
+      decorator = CapitalizeDecorator.new(person)
+      expect(decorator.correct_name).to eq('John doe')
+    end
+  end
+end
+RSpec.describe TrimmerDecorator do
+  describe '#correct_name' do
+    it 'returns the correct trimmed name for a person' do
+      person = Person.new('1', 25, 'John Doe is a very long name')
+      decorator = TrimmerDecorator.new(person)
+      expect(decorator.correct_name).to eq('John Doe i...')
     end
   end
 end
--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,28 +1,43 @@
 require_relative 'imports'
 
-RSpec.describe Classroom do
-  let(:classroom) { Classroom.new('Math 101') }
-  let(:student1) { Student.new(1, 16, classroom.label) }
-  let(:student2) { Student.new(2, 17, classroom.label) }
+RSpec.describe Rental do
+  let(:person) { Person.new(1, 16, 'John Doe') }
+  let(:book) { Book.new('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams') }
+  let(:rental) { Rental.new('2023-04-20', book, person) }
   describe '#initialize' do
-    it 'creates a new classroom with the given label and an empty array of students' do
-      expect(classroom.label).to eq('Math 101')
-      expect(classroom.students).to eq([])
+    it 'creates a new rental with the given date, book, and person' do
+      expect(rental.date).to eq('2023-04-20')
+      expect(rental.book).to eq(book)
+      expect(rental.person).to eq(person)
     end
   end
-  describe '#add_student' do
-    it 'adds the given student to the classroom and sets the student\'s classroom attribute' do
-      classroom.add_student(student1)
-      expect(classroom.students).to eq([student1])
-      expect(student1.classroom).to eq(classroom)
-      classroom.add_student(student2)
-      expect(classroom.students).to eq([student1, student2])
-      expect(student2.classroom).to eq(classroom)
+  describe '#date' do
+    it 'returns the rental date' do
+      expect(rental.date).to eq('2023-04-20')
     end
-    it 'does not add the same student to the classroom more than once' do
-      classroom.add_student(student1)
-      classroom.add_student(student1)
-      expect(classroom.students).to eq([student1])
+  end
+  describe '#book' do
+    it 'returns the rented book' do
+      expect(rental.book).to eq(book)
+    end
+  end
+  describe '#person' do
+    it 'returns the person who rented the book' do
+      expect(rental.person).to eq(person)
+    end
+  end
+  describe '#person=' do
+    it 'updates the person who rented the book' do
+      new_person = Person.new(2, 18, 'Jane Doe')
+      rental.person = new_person
+      expect(rental.person).to eq(new_person)
+    end
+  end
+  describe '#book=' do
+    it 'updates the rented book' do
+      new_book = Book.new('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling')
+      rental.book = new_book
+      expect(rental.book).to eq(new_book)
     end
   end
 end
