1a3e3c7f3379f9d9458705c2ba7e11cb682bb943
kdan2944 <kdan2944gmail.com>
2023-04-20 15:53:02 +0300

    Modify class.rb

---
+++ lib/classroom.rb
@@ -0,0 +1,15 @@
+class Classroom
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    return if @students.include?(student)
+
+    @students << student
+    student.classroom = self
+  end
+end
